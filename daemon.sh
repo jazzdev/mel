ea682708cf229043cf62cb2bdeb9a4cfbd35de9a
JD Brennan <jazzdev@gmail.com>
2012-11-05 19:08:18 -0800

    Added startup script

---
+++ daemon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export PATH=$PATH:/usr/local/bin/
+
+DIR=`dirname $0`
+COFFEE=/usr/local/bin/coffee
+HOST=irc.docusignhq.com
+LOGIN=`perl -MNet::Netrc -e "print Net::Netrc->lookup('$HOST')->login"`
+PASS=`perl -MNet::Netrc -e "print Net::Netrc->lookup('$HOST')->password"`
+
+$COFFEE $DIR/mel.coffee \
+  -s $HOST -c \#Martini --ssl \
+  -u $LOGIN -p $PASS >> $DIR/daemon.log 2>&1
